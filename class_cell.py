dbbaf81141fa81d3259fdef88887d3b097b0485a
Andreu Lorenzo <lorenzo.andreu@laplateforme.io>
2025-03-28 11:22:55 +0100

    Game_logic

---
+++ class_cell.py
@@ -0,0 +1,17 @@
+class Cell:
+    def __init__(self):
+        self.revealed = False
+        self.mine = False
+        self.adjacent_mines = 0
+    
+    def reveal(self):
+        self.revealed = True
+
+
+
+
+
+
+
+
+
